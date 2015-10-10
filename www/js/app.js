d7bbaebfedfd10c950dc94e660baa350601c8c78
6Mario6 <mario66fernandez@gmail.com>
2015-10-10 14:53:27 -0500

    conexion con firebase

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('upetapp', ['ionic', 'upetapp.controllers'])
+angular.module('upetapp', ['ionic','firebase', 'upetapp.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$rootScope, $firebaseAuth, $firebase, $window, $ionicLoading) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,17 +19,74 @@ angular.module('upetapp', ['ionic', 'upetapp.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+        $rootScope.userEmail = null;
+        $rootScope.displayname = null;
+        $rootScope.baseUrl = "https://udeamascotas.firebaseio.com/";
+        var authRef = new Firebase($rootScope.baseUrl);
+        $rootScope.auth = $firebaseAuth(authRef);
+
+        $rootScope.show = function(text) {
+            text = text || 'Cargando...';
+            $rootScope.loading = $ionicLoading.show({
+            template: text
+            });
+        };
+
+        $rootScope.hide = function() {
+            $ionicLoading.hide();
+        };
+
+        $rootScope.notify = function(text) {
+            $rootScope.show(text);
+            $window.setTimeout(function() {
+                $rootScope.hide();
+            }, 1999);
+        };
+
+        $rootScope.logout = function() {
+            $rootScope.auth.$logout();
+            $rootScope.checkSession();
+            $rootScope.show('Sesión cerrada con éxito!');
+            $window.setTimeout(function() {
+                $rootScope.hide();
+            }, 1999);
+
+        };
+
+        $rootScope.checkSession = function() {
+            var auth = new FirebaseSimpleLogin(authRef, function(error, user) {
+                if (error) {
+                    // no action yet.. redirect to default route
+                    $rootScope.userEmail = null;
+                    $window.location.href = '#/login';
+                } else if (user) {
+                    // user authenticated with Firebase
+                    $rootScope.userEmail = user.email;
+                    $window.location.href = ('#/app/mascota');
+                } else {
+                    // user is logged out
+                    $rootScope.userEmail = null;
+                    $window.location.href = '#/login';
+                }
+            });
+        }
+
+
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-
+    .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl'
+    })
     .state('app', {
     url: '/app',
     abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    templateUrl: 'templates/menu.html'
+  
   })
 
   .state('app.mascota', {
@@ -37,7 +94,7 @@ angular.module('upetapp', ['ionic', 'upetapp.controllers'])
     views: {
       'menuContent': {
         templateUrl: 'templates/mascota.html',
-        controller:'ListPetCtrl'
+        controller:'myListCtrl'
       }
     }
   })
@@ -51,5 +108,5 @@ angular.module('upetapp', ['ionic', 'upetapp.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/mascota');
+  $urlRouterProvider.otherwise('/login');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,55 +1,194 @@
 angular.module('upetapp.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
 .controller('ListPetCtrl', ['$scope','$http','$state',function($scope,$http,$state){
   $http.get('js/data.json').success(function(data){
     $scope.pets=data.pets;
-   
+    
     $scope.whichpets=$state.params.aId;
     $scope.data={showReorder:false};
     $scope.moveItem= function(item, fromIndex, toIndex){
       $scope.pets.splice(fromIndex,1);
-       $scope.pets.splice(toIndex,0,item);
+      $scope.pets.splice(toIndex,0,item);
     };
   });
-}]);
+}])
+
+.controller('LoginCtrl', [
+  '$scope', '$rootScope', '$ionicModal','$firebaseAuth', '$window',
+  function ($scope, $rootScope,$ionicModal, $firebaseAuth, $window)  {
+    $rootScope.checkSession();
+    $scope.user = {
+      email: "",
+      password: "",
+      displayName:""
+    };
+    $ionicModal.fromTemplateUrl('templates/signup.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+    $scope.validateUser = function() {
+      $rootScope.show('Por favor espere..');
+      var email = this.user.email;
+      var password = this.user.password;
+      if (!email || !password) {
+        $rootScope.notify("Por favor ingrese datos validos");
+        return false;
+      }
+      $rootScope.auth.$login('password', {
+        email: email,
+        password: password
+      }).then(function(user) {
+        $rootScope.hide();
+        $rootScope.userEmail = user.email;
+        alert("se guardo"+ $rootScope.userEmail);
+        $window.location.href = ('#/app/mascota');
+      }, function(error) {
+        $rootScope.hide();
+        if (error.code == 'INVALID_EMAIL') {
+          $rootScope.notify('Email incorrecto');
+        } else if (error.code == 'INVALID_PASSWORD') {
+          $rootScope.notify('Password incorrecto');
+        } else if (error.code == 'INVALID_USER') {
+          $rootScope.notify('Usuario Incorrecto');
+        } else {
+          $rootScope.notify('Oops something went wrong. Please try again later');
+        }
+      });
+    }
+
+    $scope.createUser = function() {
+      var email = this.user.email;
+      var password = this.user.password;
+      var displayName =this.user.displayName;
+      if (!email || !password|| !displayName) {
+        $rootScope.notify("Por favor ingrese datos validos");
+        return false;
+      }
+      $rootScope.show('Por favor espere.. Registrando');
+
+      $rootScope.auth.$createUser(email, password, function(error, user) {
+        if (!error) {
+          $rootScope.hide();
+          $scope.modal.hide();
+          $rootScope.userEmail = user.email;
+          $rootScope.displayname = user.displayName;
+          $window.location.href = ('#/app/mascota');
+        } else {
+          $rootScope.hide();
+          if (error.code == 'INVALID_EMAIL') {
+            $rootScope.notify('Invalid Email Address');
+          } else if (error.code == 'EMAIL_TAKEN') {
+            $rootScope.notify('Email ya existe');
+          } else {
+            $rootScope.notify('Oops something went wrong. Please try again later');
+          }
+        }
+      });
+    }
+
+  }
+  ])
+.controller('myListCtrl', function($rootScope, $scope, $window, $ionicModal, $firebase) {
+  $rootScope.show("Por favor espere... Procesando");
+  $scope.pets = [];
+  var petListRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+  petListRef.on('value', function(snapshot) {
+    var data = snapshot.val();
+    $scope.pets = [];
+
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+          data[key].key = key;
+          $scope.pets.push(data[key]);
+      }
+    }
+
+    if ($scope.pets.length == 0) {
+      $scope.noData = true;
+    } else {
+      $scope.noData = false;
+    }
+    $rootScope.hide();
+  });
+
+  $ionicModal.fromTemplateUrl('templates/newPet.html', function(modal) {
+    $scope.newTemplate = modal;
+  });
+
+  $scope.newPet = function() {
+    $scope.newTemplate.show();
+  };
+
+  $scope.deleteItem = function(key) {
+    $rootScope.show("Please wait... Deleting from List");
+    var itemRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+    bucketListRef.child(key).remove(function(error) {
+      if (error) {
+        $rootScope.hide();
+        $rootScope.notify('Oops! something went wrong. Try again later');
+      } else {
+        $rootScope.hide();
+        $rootScope.notify('Successfully deleted');
+      }
+    });
+  };
+})
+.controller('newCtrl', function($rootScope, $scope, $window, $firebase) {
+  $scope.data = {
+    pet: "",
+    id:"",
+    owner:"",
+    species:"",
+    breed:"",
+    gender:"",
+    birthdate:""
+
+  };
+
+  $scope.close = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.createNew = function() {
+    var pet = this.data.pet;
+    var id = this.data.id;
+    var owner = this.data.owner;
+    var species = this.data.species;
+    var breed = this.data.breed;
+    var gender = this.data.gender;
+    var birthdate = this.data.birthdate;
+    if (!pet || !id|| !owner || !species|| !breed|| !gender|| !birthdate) {
+      $rootScope.notify("Por favor ingrese datos validos");
+      return false;
+    }
+
+    $scope.modal.hide();
+    $rootScope.show();
+
+    $rootScope.show("Por favor espere... Registrando mascota");
+
+    var form = {
+      pet: pet,
+      id:id,
+      owner:owner,
+      species:species,
+      breed:breed,
+      gender:gender,
+      birthdate:birthdate
+    };
+
+    var petListRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+    $firebase(petListRef).$add(form);
+    $rootScope.hide();
+
+  };
+})
+;
+function escapeEmailAddress(email) {
+  if (!email) return false
+    // Replace '.' (not allowed in a Firebase key) with ','
+  email = email.toLowerCase();
+  email = email.replace(/\./g, ',');
+  return email.trim();
+}
