3e4d425dc0bd2f8fc5a4a64eb2c13962f351fde3
6Mario6 <mario66fernandez@gmail.com>
2015-10-21 22:56:46 -0500

    Se agregaron vistas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,7 +41,6 @@ angular.module('upetapp.controllers', [])
       }).then(function(user) {
         $rootScope.hide();
         $rootScope.userEmail = user.email;
-        alert("se guardo"+ $rootScope.userEmail);
         $window.location.href = ('#/app/mascota');
       }, function(error) {
         $rootScope.hide();
@@ -184,6 +183,198 @@ angular.module('upetapp.controllers', [])
 
   };
 })
+.controller('MapController', function($scope, uiGmapGoogleMapApi) {
+     
+    $scope.myLocation = {
+    lng : '',
+    lat: ''
+  }
+  
+  $scope.drawMap = function(position) {
+
+    //$scope.$apply is needed to trigger the digest cycle when the geolocation arrives and to update all the watchers
+    $scope.$apply(function() {
+      $scope.myLocation.lng = position.coords.longitude;
+      $scope.myLocation.lat = position.coords.latitude;
+
+      $scope.map = {
+        center: {
+          latitude: $scope.myLocation.lat,
+          longitude: $scope.myLocation.lng
+        },
+        zoom: 14,
+        pan: 1
+      };
+
+      $scope.marker = {
+        id: 0,
+        coords: {
+          latitude: $scope.myLocation.lat,
+          longitude: $scope.myLocation.lng
+        }
+      }; 
+      
+      $scope.marker.options = {
+        draggable: false,
+        labelContent: "lat: " + $scope.marker.coords.latitude + '<br/> ' + 'lon: ' + $scope.marker.coords.longitude,
+        labelAnchor: "80 120",
+        labelClass: "marker-labels"
+      };  
+    });
+  }
+
+  navigator.geolocation.getCurrentPosition($scope.drawMap);                
+})
+.controller('DashCtrl', function ($scope, $ionicPopup) {
+
+        $scope.slots = [
+            {epochTime: 12600, step: 15, format: 12},
+            {epochTime: 54900, step: 1, format: 24}
+        ];
+
+        $scope.showTimePickerModal = function (obj) {
+
+            $scope.time = { hours: 0, minutes: 0, meridian: "" };
+
+            var objDate = new Date(obj.epochTime * 1000);       // Epoch time in milliseconds.
+
+            $scope.increaseHours = function () {
+                if (obj.format == 12) {
+                    if ($scope.time.hours != 12) {
+                        $scope.time.hours += 1;
+                    } else {
+                        $scope.time.hours = 1;
+                    }
+                }
+                if (obj.format == 24) {
+                    if ($scope.time.hours != 23) {
+                        $scope.time.hours += 1;
+                    } else {
+                        $scope.time.hours = 0;
+                    }
+                }
+            };
+
+            $scope.decreaseHours = function () {
+                if (obj.format == 12) {
+                    if ($scope.time.hours > 1) {
+                        $scope.time.hours -= 1;
+                    } else {
+                        $scope.time.hours = 12;
+                    }
+                }
+                if (obj.format == 24) {
+                    if ($scope.time.hours > 0) {
+                        $scope.time.hours -= 1;
+                    } else {
+                        $scope.time.hours = 23;
+                    }
+                }
+            };
+
+            $scope.increaseMinutes = function () {
+                if ($scope.time.minutes != (60 - obj.step)) {
+                    $scope.time.minutes += obj.step;
+                } else {
+                    $scope.time.minutes = 0;
+                }
+            };
+
+            $scope.decreaseMinutes = function () {
+                if ($scope.time.minutes != 0) {
+                    $scope.time.minutes -= obj.step;
+                } else {
+                    $scope.time.minutes = 60 - obj.step;
+                }
+            };
+
+            if (obj.format == 12) {
+
+                $scope.time.meridian = (objDate.getUTCHours() >= 12) ? "PM" : "AM";
+                $scope.time.hours = (objDate.getUTCHours() > 12) ? ((objDate.getUTCHours() - 12)) : (objDate.getUTCHours());
+                $scope.time.minutes = (objDate.getUTCMinutes());
+
+                if ($scope.time.hours == 0 && $scope.time.meridian == "AM") {
+                    $scope.time.hours = 12;
+                }
+
+                $scope.changeMeridian = function () {
+                    $scope.time.meridian = ($scope.time.meridian === "AM") ? "PM" : "AM";
+                };
+
+                $ionicPopup.show({
+                    templateUrl: 'my-time-picker-12-hour.tpl.html',
+                    title: '<strong>12-Hour Format</strong>',
+                    subTitle: '',
+                    scope: $scope,
+                    buttons: [
+                        { text: 'Cancel' },
+                        {
+                            text: 'Set',
+                            type: 'button-positive',
+                            onTap: function (e) {
+
+                                $scope.loadingContent = true;
+
+                                var totalSec = 0;
+
+                                if ($scope.time.hours != 12) {
+                                    totalSec = ($scope.time.hours * 60 * 60) + ($scope.time.minutes * 60);
+                                } else {
+                                    totalSec = $scope.time.minutes * 60;
+                                }
+
+                                if ($scope.time.meridian === "AM") {
+                                    totalSec += 0;
+                                } else if ($scope.time.meridian === "PM") {
+                                    totalSec += 43200;
+                                }
+                                obj.epochTime = totalSec;
+
+                            }
+                        }
+                    ]
+                })
+
+            }
+
+            if (obj.format == 24) {
+
+                $scope.time.hours = (objDate.getUTCHours());
+                $scope.time.minutes = (objDate.getUTCMinutes());
+
+                $ionicPopup.show({
+                    templateUrl: 'my-time-picker-24-hour.tpl.html',
+                    title: '<strong>24-Hour Format</strong>',
+                    subTitle: '',
+                    scope: $scope,
+                    buttons: [
+                        { text: 'Cancel' },
+                        {
+                            text: 'Set',
+                            type: 'button-positive',
+                            onTap: function (e) {
+
+                                $scope.loadingContent = true;
+
+                                var totalSec = 0;
+
+                                if ($scope.time.hours != 24) {
+                                    totalSec = ($scope.time.hours * 60 * 60) + ($scope.time.minutes * 60);
+                                } else {
+                                    totalSec = $scope.time.minutes * 60;
+                                }
+                                obj.epochTime = totalSec;
+                            }
+                        }
+                    ]
+                })
+
+            }
+
+        };
+
+    })
 ;
 function escapeEmailAddress(email) {
   if (!email) return false
